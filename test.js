fe051e769cc7e378424386738d2a8acdf88574db
axing521 <axing@bupt.edu.cn>
2023-04-07 15:27:49 +0800

    new file:   demo.html modified:   test.js

--- test.js
+++ test.js
@@ -1,14 +1,267 @@
 /***
  * @creater:ACBash
- * @create_time:22-11-21 16:21:26
+ * @create_time:23-3-30 14:44:18
  * @last_modify:ACBash
- * @modify_time:23-2-26 17:51:54
- * @line_count:6
+ * @modify_time:23-4-3 11:8:54
+ * @line_count:259
  **/
 
-let a = {"1": 2};
-let b = {"1": 2};
+let a = {
+  "columns": [
+    {
+      "field": "course_img",
+      "title": "图片",
+      "showOverflow": true,
+      "id": 1,
+      "editRender": {
+        "name": "img",
+        "attrs": {
+          "src": "https://file.xdclass.net/video/2022/77-QD/cover.jpeg"
+        },
+        "enabled": false
+      },
+      "translatableKey": "app.Page-7AVwmK16801424432360434.editTable_w1KN9J16801557346430400.1680156514361_0",
+      "visible": true,
+      "showColumn": false,
+      "tableBold": false,
+      "className": "  ",
+      "tableItalics": false,
+      "tableUnderline": false
+    },
+    {
+      "field": "title",
+      "title": "标题",
+      "showOverflow": true,
+      "id": 2,
+      "editRender": {
+        "name": "input",
+        "attrs": {
+          "placeholder": ""
+        },
+        "enabled": false
+      },
+      "translatableKey": "app.Page-7AVwmK16801424432360434.editTable_w1KN9J16801557346430400.1680156514381_1"
+    },
+    {
+      "field": "price",
+      "title": "价格",
+      "showOverflow": true,
+      "id": 3,
+      "editRender": {
+        "name": "input",
+        "attrs": {
+          "placeholder": ""
+        },
+        "enabled": false
+      },
+      "translatableKey": "app.Page-7AVwmK16801424432360434.editTable_w1KN9J16801557346430400.1680156514396_2"
+    },
+    {
+      "title": "评分",
+      "field": "point",
+      "showOverflow": true,
+      "width": "",
+      "editRender": {
+        "name": "input",
+        "attrs": {
+          "placeholder": ""
+        },
+        "enabled": false
+      },
+      "showColumn": false,
+      "tagHover": false,
+      "id": "columns16801565145993",
+      "fixed": "",
+      "translatableKey": "app.Page-7AVwmK16801424432360434.editTable_w1KN9J16801557346430400.1680156527246_3"
+    },
+    {
+      "title": "操作",
+      "field": "columns16801565169764",
+      "showOverflow": true,
+      "width": "",
+      "editRender": {
+        "name": "$input",
+        "attrs": {
+          "placeholder": ""
+        },
+        "enabled": false
+      },
+      "showColumn": false,
+      "tagHover": false,
+      "id": "columns16801565169764",
+      "fixed": "",
+      "translatableKey": "app.Page-7AVwmK16801424432360434.editTable_w1KN9J16801557346430400.1680156527257_4"
+    }
+  ],
+  "data": [
+    {
+      "id": 10001,
+      "src": "https://file.xdclass.net/video/2022/77-QD/cover.jpeg",
+      "email": "T1",
+      "role": "Develop",
+      "title": "22年新版【前端高级工程师】面试专题第一季",
+      "age": 0,
+      "price": "99",
+      "point": 9.8,
+      "categoty": "front",
+      "del": 0
+    },
+    {
+      "id": 10002,
+      "name": "Anna",
+      "email": "T2",
+      "role": "Test",
+      "gender": "Female",
+      "age": 22,
+      "address": "Guangzhou",
+      "childMenuList": [
+        {
+          "id": 24300,
+          "name": "张婷婷",
+          "gender": "Female",
+          "address": "Guangzhou"
+        },
+        {
+          "id": 20045,
+          "name": "张芳",
+          "gender": "Female",
+          "address": "Guangzhou"
+        },
+        {
+          "id": 10053,
+          "name": "李庆",
+          "gender": "Male",
+          "address": "Shenzhen",
+          "childMenuList": [
+            {
+              "id": 24330,
+              "name": "王阳",
+              "gender": "Male",
+              "address": "Shenzhen"
+            },
+            {
+              "id": 21011,
+              "name": "文佳佳",
+              "gender": "Female",
+              "address": "Shenzhen"
+            },
+            {
+              "id": 22200,
+              "name": "萧蔷",
+              "gender": "Female",
+              "address": "Shenzhen"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "id": 10003,
+      "name": "Tom",
+      "email": "T3",
+      "role": "PM",
+      "gender": "Male",
+      "age": 100,
+      "address": "Shanghai"
+    },
+    {
+      "id": 10004,
+      "name": "小米",
+      "email": "T4",
+      "role": "Designer",
+      "gender": "Female",
+      "age": 70,
+      "address": "Shenzhen"
+    },
+    {
+      "id": 10005,
+      "name": "小麦",
+      "email": "T5",
+      "role": "Develop",
+      "gender": "Female",
+      "age": 10,
+      "address": "Shanghai"
+    },
+    {
+      "id": 10006,
+      "name": "老李",
+      "email": "T6",
+      "role": "Designer",
+      "gender": "Female",
+      "age": 90,
+      "address": "Shenzhen"
+    },
+    {
+      "id": 10007,
+      "name": "小白",
+      "email": "T7",
+      "role": "Test",
+      "gender": "Male",
+      "age": 5,
+      "address": "Shenzhen"
+    },
+    {
+      "id": 10008,
+      "name": "老王",
+      "email": "T8",
+      "role": "Develop",
+      "gender": "Male",
+      "age": 80,
+      "address": "Shenzhen"
+    },
+    {
+      "id": 10008,
+      "name": "Test9",
+      "email": "T8",
+      "role": "Develop",
+      "gender": "Male",
+      "age": 80,
+      "address": "Shenzhen"
+    },
+    {
+      "id": 10009,
+      "name": "Test10",
+      "email": "T8",
+      "role": "Develop",
+      "gender": "Male",
+      "age": 80,
+      "address": "Shenzhen"
+    },
+    {
+      "id": 10018,
+      "name": "Test11",
+      "email": "T8",
+      "role": "Develop",
+      "gender": "Male",
+      "age": 80,
+      "address": "Shenzhen"
+    },
+    {
+      "id": 10208,
+      "name": "Test12",
+      "email": "T8",
+      "role": "Develop",
+      "gender": "Male",
+      "age": 80,
+      "address": "Shenzhen"
+    },
+    {
+      "id": 100038,
+      "name": "Test13",
+      "email": "T8",
+      "role": "Develop",
+      "gender": "Male",
+      "age": 80,
+      "address": "Shenzhen"
+    }
+  ]
+}
 
-console.log(a == b);
-
-console.log(Math.ceil(2.2));
+axios.post(url, { param: { 'd.key': from_to }, })
+     .then((res) => { 
+            res.data.data.rows.forEach(element => { 
+                quarter_from_to.push(element.value); 
+            }); 
+            console.log(quarter_from_to); 
+            setVariable('quarter_from_to', quarter_from_to); 
+          }); 
